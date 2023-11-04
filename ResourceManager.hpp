ac32b6003b14ff791df678302b540bb8e76d06d9
Weronika <01148928@pw.edu.pl>
2023-11-04 12:59:14 +0100

    lab2

--- ResourceManager.hpp
+++ ResourceManager.hpp
@@ -4,5 +4,53 @@
 
 class ResourceManager
 {
-    // Twoja implementacja tutaj
+public:
+
+    ResourceManager()
+    {
+        resource = new Resource();
+    }
+
+    ResourceManager(const ResourceManager& other)
+    {
+        resource = new Resource(*other.resource); 
+    }
+
+    ResourceManager(ResourceManager&& other)
+    {
+        resource = other.resource;
+        other.resource = nullptr;
+    }
+
+    ResourceManager& operator=(const ResourceManager& other)
+    {
+        delete resource;
+        resource = new Resource(*other.resource);
+        return *this;
+    }
+
+    ResourceManager& operator=(ResourceManager&& other) noexcept
+    {
+        delete resource;
+        resource = other.resource;
+        other.resource = nullptr;
+        return *this;
+    }
+
+    ~ResourceManager()
+    {
+        if(resource != nullptr)
+        {
+            delete resource;
+        }
+    }
+
+    double get()
+    {
+        return resource->get();
+    }
+
+
+private:
+    Resource* resource;
 };
